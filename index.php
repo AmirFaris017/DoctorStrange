3402f9858a4cf15ef1617408d74a0e37dd470e6d
AmirFaris017 <2021823142@student.uitm.edu.my>
2023-05-25 06:36:52 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    $username = $_POST['username'];
+    $username = $_POST['email'];
+    $username = $_POST['password'];
+    $username = $_POST['address'];
+
+
+    $conn = new mysqli('localhost','root','',homestay);
+    if($conn->connect_error){
+    die('Connection Failed : ' $conn->connect_error);
+    }else
+
+        $stmt = $conn->prepare("insert into registration(username,email,password,address)")values (?,?,?,?)");
+
+    
+    
+?>
+
+
+
+   
